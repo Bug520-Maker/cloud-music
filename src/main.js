6a0d8d54bcf70e3549f65ee71bd6d7fbc02ed8cb
Bug520-Maker <betweent@126.com>
2021-02-01 22:55:54 +0800

    初步实现下拉加载更多

--- src/main.js
+++ src/main.js
@@ -3,19 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueLazyload from "vue-lazyload";
-/*轮播图相关插件配置*/
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import { Swiper as SwiperClass, Pagination, Mousewheel, Autoplay } from 'swiper/swiper.esm'
-import getAwesomeSwiper from 'vue-awesome-swiper/dist/exporter'
-SwiperClass.use([Pagination, Mousewheel, Autoplay])
-Vue.use(getAwesomeSwiper(SwiperClass))
-const { Swiper, SwiperSlide } = getAwesomeSwiper(SwiperClass)
-import 'swiper/swiper-bundle.css'
 
 /*element-ui*/
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(VueAwesomeSwiper);
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.use(VueLazyload, {
